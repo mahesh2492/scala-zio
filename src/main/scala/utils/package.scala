d7f492160531aacfcaa31b2b9d9dd9b868d6047c
Mahesh Chand <mahesh.kndpl@gmail.com>
2024-04-23 17:33:01 +0530

    fibers

---
+++ src/main/scala/utils/package.scala
@@ -0,0 +1,12 @@
+import zio._
+
+package object utils {
+
+  implicit class DebugWrapper[R,E,A](zio: ZIO[R,E,A]) {
+    def debugThread: ZIO[R,E,A] =
+      zio
+        .tap(a => ZIO.succeed(println(s"[${Thread.currentThread().getName}] $a")))
+        .tapErrorCause(cause => ZIO.succeed(println(s"[${Thread.currentThread().getName}][FAIL] $cause")))
+  }
+
+}
